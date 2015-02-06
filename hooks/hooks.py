790424cd1b6abb3b3ca6d284205ca50deb1e27da
Whit Morriss <whit.morriss@canonical.com>
2015-02-06 06:05:18 -0600

    initial commit, take 2

---
+++ hooks/hooks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from charmhelpers.contrib.ansible import AnsibleHooks
+from path import path
+import sys
+
+
+def hook_names(here=path(__file__).parent):
+    for name in (x.basename() \
+                 for x in here.files() if x.islink()):
+        yield name
+
+hooks = AnsibleHooks(playbook_path='playbook/main.yml',
+                     default_hooks=list(hook_names))
+
+if __name__ == "__main__":
+    hooks.execute(sys.argv)
+
+# @hooks.hook()
+# def install():
+#     """
+#     Bootstrap ansible before running playbook
+#     """
+#     try:
+#         import ansible
+#     except ImportError:
+#         install_ansible_support(from_ppa=True)
